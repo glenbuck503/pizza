20e4ece73e6f7f05c56725b80bb627adbf886886
glenbuck503 <glenbuck503@gmail.com>
2020-11-06 14:56:49 -0800

    add addvalue function to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,44 @@
 function Pizza(size,toppings) {
   this.size = size;
   this.toppings = toppings
-
-
 }
 
-let price = 2;
+let price = 3;
 
+Pizza.prototype.addValue = function (size, topping1, topping2, topping3) {
+  let price = size + topping1 + topping2 + topping3
+  this.price = price;
+}
+
+console.log(price)
 
 
 
 
 
 $(document).ready(function() {
-  $("form#pizza").submit(function (event)  {
+  $("form#pizzaOrder").submit(function (event)  {
     event.preventDefault();
 
-    $( "h2").empty();
+   
+
+    
 
     let pizza = new Pizza();
+    
     const size = parseInt($("select#size").val());
-    const sauce = parseInt($("select#sauce").val());
-    const saucename = $("#sauce option:selected").text();
+    
+    const parseTop1 = parseInt($("select#topping1").val());
+    const topping1 = $("#topping2 option:selected").text();
 
-    const cheese = parseInt($("select#cheese").val());
-    const cheesename = $("#cheese option:selected").text();
+    const parseTop2 = parseInt($("select#topping2").val()); 
+    const topping2 = $("#topping2 option:selected").text();
+    
+    const parseTop3 = parseInt($("select#topping3").val());
+    const topping3 = $("#topping3 option:selected").text();
 
-    const extra1 = parseInt($("select#extras1").val());
-    const extra1name = $("#extras1 option:selected").text();
-
-    const extra2 = parseInt($("select#extras2").val()); 
-    const extra2name = $("#extras2 option:selected").text();
-
-    const extra3 = parseInt($("select#extras3").val());
-    const extra3name = $("#extras3 option:selected").text();
-
-    pizza.size(size);
-    pizza.addValue(size, sauce, cheese, extra1, extra2, extra3);
-    pizza.showPizza(cheesename, saucename, extra1name, extra2name, extra3name)
+   
+    pizza.addValue(size + topping1 + topping2 + topping3);
+    // pizza.showPizza(cheesename, saucename, extra1name, extra2name, extra3name)
   });
 });
