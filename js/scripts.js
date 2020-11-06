439fc8a411139f9129bb34da941a913415b683b0
glenbuck503 <glenbuck503@gmail.com>
2020-11-06 10:47:35 -0800

    add consts to jquerygit

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,25 @@ function Pizza(size,toppings){
   this.size = size;
   this.toppings = toppings;
 }
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+  $("#orderForm").submit(function(event){
+    event.preventDefault();
+    var userSize = parseFloat($("#userSize").val());
+    var userTopping = parseFloat($("#firstTopping").val());
+    var userTopping2 = parseFloat($("#secondTopping").val());
+    var userTopping3 = parseFloat($("#thirdTopping").val());
+
+    var userPizza = new Pizza(size,toppings1,toppings2,toppings3);
+    $("#output").show()
+    $("#output").text("dsfsd");
+  });
+});
