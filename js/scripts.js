582b7c263091e538076e86560d62645aee512504
glenbuck503 <glenbuck503@gmail.com>
2020-11-06 14:40:36 -0800

    add pizza function

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,42 @@
-function Pizza(size,userTopping1,userTopping2,userTopping3 ){
+function Pizza(size,toppings) {
   this.size = size;
-  this.userTopping1 = userTopping1;
-  this.userTopping2 = userTopping2;
-  this.userTopping3 = userTopping3
+  this.toppings = toppings
+
+
 }
 
-Pizza.prototype.price = function() {
-  const total = this.userTopping1 + this.userTopping2 + this.userTopping3 + this.size;
-
-  return total;
- 
-}
+let price = 2;
 
 
+
+
+
+
+$(document).ready(function() {
+  $("form#pizza").submit(function (event)  {
+    event.preventDefault();
+
+    $( "h2").empty();
+
+    let pizza = new Pizza();
+    const size = parseInt($("select#size").val());
+    const sauce = parseInt($("select#sauce").val());
+    const saucename = $("#sauce option:selected").text();
+
+    const cheese = parseInt($("select#cheese").val());
+    const cheesename = $("#cheese option:selected").text();
+
+    const extra1 = parseInt($("select#extras1").val());
+    const extra1name = $("#extras1 option:selected").text();
+
+    const extra2 = parseInt($("select#extras2").val()); 
+    const extra2name = $("#extras2 option:selected").text();
+
+    const extra3 = parseInt($("select#extras3").val());
+    const extra3name = $("#extras3 option:selected").text();
+
+    pizza.size(size);
+    pizza.addValue(size, sauce, cheese, extra1, extra2, extra3);
+    pizza.showPizza(cheesename, saucename, extra1name, extra2name, extra3name)
+  });
+});
