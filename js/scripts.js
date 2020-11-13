beab557e7986d39d3581adf6cd8f6c5542ca3ed4
glenbuck503 <glenbuck503@gmail.com>
2020-11-12 19:40:18 -0800

    create new prototype. Delete addToppings

--- js/scripts.js
+++ js/scripts.js
@@ -4,26 +4,20 @@ function Pizza(size, toppings) {
   
 }
 
-
-// Pizza.prototype.addPrice = function() {
-//   let total = 5;
-//   if (this.size === "12") {
-//     total += 5;
-//   } else if (this.size === "14") {
-//     total += 7;
-//   } else if (this.size === "16") {
-//     total += 9;
-//   }
-//   else {
-//     total += 0;
-//   }
-//   for (let i=0; i < this.toppings.length; i++) {
-//     total +=1;
-//   }
-//   return total;
-  
-// };
-
+Pizza.prototype.addPrice = function() {
+  let total = 10;
+  if (this.size === "medium") {
+    total += 4;
+  } else if (this.size === "large") {
+    total += 6;
+  } else {
+    total += 0;
+  }
+  for (let i=0; i < this.toppings.length; i++) {
+    total +=1;
+  }
+  return total;
+};
 
 
 Pizza.prototype.addValue = function ( size) {
@@ -32,26 +26,12 @@ Pizza.prototype.addValue = function ( size) {
   
 }
 
-Pizza.prototype.addToppings = function (toppings) {
-  this.toppings = toppings;
-}
 
-// Pizza.prototype.costCalculator = function(){
-//   if (this.size === "Large") {
-//     this.cost += 5; 
-//   }
-//   if (this.toppings.length > 1) {
-//     this.cost += 5; 
-//   }
-//   return this.cost; 
-// }
 
-Pizza.prototype.displayPizza = function(size, topping1, topping2, topping3) {
 
-  let toppingDisplay = size + topping1 + topping2 + topping3;
-  $("#final-price").text("The cost of your pizza is " + toppingDisplay);
 
-}
+
+
 
 
 
@@ -62,30 +42,21 @@ $(document).ready(function() {
 
     let size = $("select#userSize");
     let toppings = [];
+    $("input:checkbox[name=topping]:checked").each(function() {
+      toppings.push($(this).val());
+    });
 
-   
-
-    
 
     let pizza = new Pizza();
+    let price = pizza.addPrice();
     
-    
-    const size = $("#userSize option:selected").val();
-    
-    
-    const parseTop1 = parseInt($("select#topping1").val());
-    const topping1 = $("#topping1 option:selected").val();
-
-    const parseTop2 = parseInt($("select#topping2").val()); 
-    const topping2 = $("#topping2 option:selected").val();
-    
-    const parseTop3 = parseInt($("select#topping3").val());
-    const topping3 = $("#topping3 option:selected").val();
-
    
-   pizza.addValue(size,parseTop1, parseTop2, parseTop3);
-    pizza.displayPizza(size, topping1, topping2, topping3);
-    $("#customerOrder").show();
-   $("#final-price").text()
+    
+    
+    
+   
+   
+    $("#output").show();
+   
   });
 });
